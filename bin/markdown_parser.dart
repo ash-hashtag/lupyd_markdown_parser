548cbfda7ddb99160f972e49f9821fd2533a5d2d
ash-hashtag <hashtag438@gmail.com>
2023-05-16 17:30:53 +0530

    works great

--- bin/markdown_parser.dart
+++ bin/markdown_parser.dart
@@ -1,92 +1,16 @@
-import 'makrdown_parser_base.dart';
+import 'package:markdown_parser/markdown_parser.dart';
 
 void main(List<String> arguments) {
-  defaultTest();
+  test();
 }
 
-// void defaultTest() {
-//   final boldMatcher = regexPatternMatcher(RegExp(r"\*\*\*(.*?)\*\*\*"));
-//   final italicMatcher = regexPatternMatcher(RegExp(r"\/\/\/(.*?)\/\/\/"));
-//   final underlineMatcher = regexPatternMatcher(RegExp(r"___(.*?)___"));
-//   final matchers = [
-//     Tuple(ParsedPartType.bold, boldMatcher),
-//     Tuple(ParsedPartType.italic, italicMatcher),
-//   ];
-//   const inputText = "hello ***world*** ok///no///";
+void test() {
+  const inputText =
+      r"""hello @nope_dolalt ***wo///rl///d*** ok///no ***thanks***/// ___under///***bombastic*** bomba fantastic ///___ ####hashtag### ___hello___ #nope
+>| hello quoted text #hashtag
+hyperlink: [nope](www.youtube.com)   """;
 
-//   final parts = parseText([ParsedPart.fromString(inputText)], matchers);
+  final matchers = defaultMatchers();
 
-//   print(parts);
-// }
-
-// enum ParsedPartType {
-//   none,
-//   bold,
-//   italic,
-// }
-
-// class ParsedPart {
-//   final String text;
-//   final ParsedPartType type;
-
-//   const ParsedPart(this.text, this.type);
-
-//   factory ParsedPart.fromMatch(
-//       String input, Tuple<ParsedPartType, PatternMatch> match) {
-//     return ParsedPart(input.substring(match.b.start, match.b.end), match.a);
-//   }
-
-//   factory ParsedPart.fromString(String input,
-//       [ParsedPartType type = ParsedPartType.none]) {
-//     return ParsedPart(input, type);
-//   }
-
-//   @override
-//   String toString() => "{ type: $type, text: \"$text\" }";
-// }
-
-// class PatternMatch {
-//   final int start, end;
-//   const PatternMatch(this.start, this.end);
-
-//   factory PatternMatch.fromMatch(Match match) {
-//     return PatternMatch(match.start, match.end);
-//   }
-// }
-
-// typedef PatternMatcher = Iterable<PatternMatch> Function(String input);
-
-// PatternMatcher regexPatternMatcher(RegExp regExp) =>
-//     (String input) => regExp.allMatches(input).map(PatternMatch.fromMatch);
-
-// List<ParsedPart> parseText(
-//   List<ParsedPart> inputParts,
-//   List<Tuple<ParsedPartType, PatternMatcher>> patternMatchers,
-// ) {
-//   var parts = List<ParsedPart>.from(inputParts);
-
-//   var newParts = <ParsedPart>[];
-//   for (final matcher in patternMatchers) {
-//     for (final part in parts) {
-//       final input = part.text;
-//       var i = 0;
-//       for (final match in matcher.b(input)) {
-//         if (i != match.start) {
-//           newParts.add(
-//               ParsedPart(input.substring(i, match.start), ParsedPartType.none));
-//         }
-//         newParts.add(
-//             ParsedPart(input.substring(match.start, match.end), matcher.a));
-//         i = match.end;
-//       }
-//       if (i != input.length) {
-//         newParts.add(
-//             ParsedPart(input.substring(i, input.length), ParsedPartType.none));
-//       }
-//     }
-//     parts = newParts;
-//     newParts = [];
-//   }
-
-//   return parts;
-// }
+  defaultTest(inputText, matchers);
+}
--- lib/markdown_parser.dart
+++ lib/markdown_parser.dart
@@ -1,3 +1,180 @@
-int calculate() {
-  return 6 * 7;
+import 'package:markdown_parser/tuple.dart';
+
+bool areListsEqual<T>(List<T> a, List<T> b) {
+  if (a == b) return true;
+  if (a.length != b.length) return false;
+  for (var i = 0; i < a.length; i++) {
+    if (a[i] != b[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+class PatternMatchPart {
+  final String text;
+  final List<String> type;
+
+  const PatternMatchPart({required this.text, this.type = const []});
+
+  @override
+  String toString() => '{ text: "$text", type: $type }';
+
+  bool isEqual(PatternMatchPart other) {
+    if (other == this) {
+      return true;
+    }
+    return other.text == text && areListsEqual(other.type, type);
+  }
+}
+
+class PatternMatcher {
+  final Iterable<Match> Function(String) matcher;
+  final String Function(String) delimiter;
+  final String type;
+  final bool lookInwards;
+  final bool singleType;
+
+  PatternMatcher(
+      {required this.type,
+      required this.matcher,
+      required this.delimiter,
+      required this.lookInwards,
+      required this.singleType});
+
+  @override
+  String toString() => "PatternMatcher { type: $type }";
+}
+
+class RegexPatternMatcher extends PatternMatcher {
+  final RegExp regex;
+  RegexPatternMatcher(
+      {required super.type,
+      required super.matcher,
+      required super.delimiter,
+      required this.regex,
+      required super.lookInwards,
+      required super.singleType});
+  factory RegexPatternMatcher.from(RegExp regExp, String type,
+      String Function(String) delimiter, bool lookInwards, bool singleType) {
+    return RegexPatternMatcher(
+        delimiter: delimiter,
+        matcher: (String input) => regExp.allMatches(input),
+        type: type,
+        regex: regExp,
+        lookInwards: lookInwards,
+        singleType: singleType);
+  }
+  @override
+  String toString() =>
+      "RegexPatternMatcher { type: $type, regex: ${regex.pattern} }";
+}
+
+List<PatternMatchPart> parseText(
+    PatternMatchPart inputPart, List<PatternMatcher> patternMatchers) {
+  final inputText = inputPart.text;
+  final parts = <PatternMatchPart>[];
+
+  if (patternMatchers.isEmpty) {
+    return [inputPart];
+  }
+
+  final patternMatches = <Tuple<Match, PatternMatcher>>[];
+  for (final patternMatcher in patternMatchers) {
+    patternMatches.addAll(
+        patternMatcher.matcher(inputText).map((e) => Tuple(e, patternMatcher)));
+  }
+
+  patternMatches.sort((a, b) => a.a.start - b.a.start);
+
+  var current = 0;
+  var currentTypes = [...inputPart.type];
+  for (final match in patternMatches) {
+    if (current > match.a.start) {
+      continue;
+    }
+    if (current < match.a.start) {
+      final part = PatternMatchPart(
+          text: inputText.substring(current, match.a.start),
+          type: currentTypes);
+      final result = parseText(part, patternMatchers);
+      parts.addAll(result);
+    }
+
+    final type = [if (!match.b.singleType) ...currentTypes, match.b.type];
+    final part = PatternMatchPart(
+        text:
+            match.b.delimiter(inputText.substring(match.a.start, match.a.end)),
+        type: type);
+    if (match.b.lookInwards) {
+      final result = parseText(part, patternMatchers);
+      parts.addAll(result);
+    } else {
+      parts.add(part);
+    }
+    current = match.a.end;
+  }
+  if (current < inputText.length) {
+    final input = inputText.substring(current);
+    final part = PatternMatchPart(text: input, type: currentTypes);
+    parts.add(part);
+  }
+
+  return parts;
+}
+
+const rawBoldRegex = r"(?<!\\)\*\*\*(.*?)(?<!\\)\*\*\*";
+const rawItalicRegex = r"(?<!\\)\/\/\/(.*?)(?<!\\)\/\/\/";
+const rawUnderlineRegex = r"(?<!\\)___(.*?)(?<!\\)___";
+const rawHeaderRegex = r"(?<!\\)###(.*?)(?<!\\)###";
+const rawHashtagRegex = r"(?<!\\)#\w+";
+const rawMentionRegex = r"(?<!\\)@\w+";
+const rawQuoteRegex = r"^>\|\s.*$";
+const rawHyperLinkRegex = r"\[(.+)\]\((.+)\)";
+
+List<PatternMatcher> defaultMatchers() {
+  final boldMatcher = RegexPatternMatcher.from(
+      RegExp(rawBoldRegex), "bold", tripleDelimiterBoth, true, false);
+  final italicMatcher = RegexPatternMatcher.from(
+      RegExp(rawItalicRegex), "italic", tripleDelimiterBoth, true, false);
+  final underlineMatcher = RegexPatternMatcher.from(
+      RegExp(rawUnderlineRegex), "underline", tripleDelimiterBoth, true, false);
+  final headerMatcher = RegexPatternMatcher.from(
+      RegExp(rawHeaderRegex), "header", tripleDelimiterBoth, true, false);
+  final hashtagMatcher = RegexPatternMatcher.from(
+      RegExp(rawHashtagRegex), "hashtag", noDelimiter, false, true);
+  final mentionMatcher = RegexPatternMatcher.from(
+      RegExp(rawMentionRegex), "mention", noDelimiter, false, true);
+  final quoteMatcher = RegexPatternMatcher.from(
+      RegExp(rawQuoteRegex, multiLine: true),
+      "quote",
+      tripleDelimiter,
+      true,
+      false);
+  final hyperLinkMatcher = RegexPatternMatcher.from(
+      RegExp(rawHashtagRegex), "hyperlink", noDelimiter, false, true);
+
+  return [
+    boldMatcher,
+    italicMatcher,
+    underlineMatcher,
+    headerMatcher,
+    hashtagMatcher,
+    mentionMatcher,
+    quoteMatcher,
+    hyperLinkMatcher,
+  ];
+}
+
+String tripleDelimiterBoth(String _) => _.substring(3, _.length - 3);
+String noDelimiter(String _) => _;
+String singleDelimiter(String _) => _.substring(1);
+String doubleDelimiter(String _) => _.substring(2);
+String tripleDelimiter(String _) => _.substring(3);
+
+void defaultTest(String input, List<PatternMatcher> matchers) {
+  final inputText = input;
+  final parts = parseText(PatternMatchPart(text: inputText), matchers);
+
+  print(parts);
 }
